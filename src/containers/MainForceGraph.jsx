84a74777306013aca6df26332918700138ede3a2
Gregory Smith <crashingwaves@fea.st>
2017-01-22 17:34:16 -0700

    Main force graph will update on new data

--- src/containers/MainForceGraph.jsx
+++ src/containers/MainForceGraph.jsx
@@ -7,57 +7,74 @@ import '../scss/main-force-graph.scss'
 class MainForceGraph extends Component {
     constructor(props) {
       super(props)
-      this.simulation = d3.forceSimulation().force("links", d3.forceLink().id(d => d.id))
-        .force("collide", d3.forceCollide().iterations(16))
-        .force("charge", d3.forceManyBody().strength(-250))
-        .force("center", d3.forceCenter(props.width / 2, props.height / 2))
-        .force("y", d3.forceY(0))
-        .force("x", d3.forceX(0))
+      this.simulation = d3.forceSimulation().force('links', d3.forceLink().id(d => d.id))
+        .force('collide', d3.forceCollide().iterations(16))
+        .force('charge', d3.forceManyBody().strength(-250))
+        .force('center', d3.forceCenter(props.width / 2, props.height / 2))
+        .force('y', d3.forceY(0))
+        .force('x', d3.forceX(0))
+        .on('tick', this.onTick.bind(this))
+    }
+
+    onTick() {
+      this.node
+        .attr('cx', d => `${d.x}px`)
+        .attr('cy', d => `${d.y}px`)
+      this.link
+        .attr('x1', d => d.source.x)
+        .attr('y1', d => d.source.y)
+        .attr('x2', d => d.target.x)
+        .attr('y2', d => d.target.y)
     }
 
     componentDidMount() {
       let { nodes, links } = this.props
       let self = d3.select('#main-graph')
       this.node = self.append('g')
-                .classed('people', true)
-                .selectAll('.person')
+        .classed('people', true)
+        .selectAll('.person')
+        .data(nodes)
+        .enter()
+        .append('circle')
+        .classed('person', true)
+        .attr('fill', 'blue')
+        .attr('r', '10')
       this.link = self.append('g')
-                .classed('links', true)
-                .selectAll('.link')
-      this.link
-            .data(links)
-            .enter()
-            .append("line")
-            .attr('stroke', 'black')
-            .classed("link", true)
+        .classed('links', true)
+        .selectAll('.link')
+        .data(links)
+        .enter()
+        .append('line')
+        .attr('stroke', 'black')
+        .classed('link', true)
 
-      this.node
-            .data(nodes)
-            .enter()
-            .append('circle')
-            .classed('person', true)
-            .attr('fill', 'blue')
-            .attr('r', '10')
-
-      this.simulation.nodes(nodes).on('tick', () => {
-        this.node
-          .attr('cx', d => d.x + 'px')
-          .attr('cy', d => (d.y - 5) + 'px')
-        this.link
-          .attr('x1', d => d.source.x)
-          .attr('y1', d => d.source.y)
-          .attr('x2', d => d.target.x)
-          .attr('y2', d => d.target.y)
-      });
-
-      this.simulation.force("links").links(links)
+      this.simulation.nodes(nodes);
+      this.simulation.force('links').links(links)
     }
 
     componentDidUpdate() {
       let { nodes, links } = this.props
-      let self = d3.select('#main-graph')
-      self.innerHTML = ''
 
+      this.node = this.node.data(nodes)
+      this.node.exit().remove()
+      this.node = this.node.enter()
+        .append('circle')
+        .classed('person', true)
+        .attr('fill', 'blue')
+        .attr('r', '10')
+        .merge(this.node)
+
+      this.link = this.link.data(links)
+      this.link.exit().remove()
+      this.link = this.link.enter()
+        .append('line')
+        .attr('stroke', 'black')
+        .classed('link', true)
+        .merge(this.link)
+
+      this.simulation.nodes(nodes)
+      this.simulation.force('links').links(links)
+      this.simulation.alpha(1).restart()
     }
 
     render() {
