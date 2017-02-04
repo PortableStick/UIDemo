43e82014d2a8016006f8bc1d06c1521456765fe2
Gregory Smith <crashingwaves@fea.st>
2017-02-04 13:43:21 -0700

    CORS crap

--- server.js
+++ server.js
@@ -20,6 +20,18 @@ const server = http.createServer((request, response) => {
             contentType = 'text/javascript';
             break;
     }
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    response.setHeader('Access-Control-Allow-Credentials', true);
 
     fs.readFile(filePath, function(error, content) {
         if (error) {
