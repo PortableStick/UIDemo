7ebee8220209d72ac2a58892db188b2708506574
Gregory Smith <crashingwaves@fea.st>
2017-02-04 12:48:00 -0700

    Add build

--- server.js
+++ server.js
@@ -3,10 +3,43 @@ const http = require('http'),
       websocket = require('ws'),
       fs = require('fs'),
       generateNewData = require('./dataRandomizer').generateNewData,
-      resetData = require('./dataRandomizer').resetData
+      resetData = require('./dataRandomizer').resetData,
+      path = require('path')
 
 const server = http.createServer((request, response) => {
+  console.log('request starting...');
 
+    var filePath = './build' + request.url;
+    if (filePath == './build/')
+        filePath += '/index.html';
+
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end();
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
 }).listen(port, () => { console.log(`Server listening on port ${port}`)})
 
 const wsserver = new websocket.Server({ server })
